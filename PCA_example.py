26ae12c26a128f376c8c7caa7f81abee405200c8
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2023-10-23 12:31:12 +0100

    Standardising the data

--- PCA_example.py
+++ PCA_example.py
@@ -1,3 +1,26 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+# load dataset into a Pandas DataFrame
+
+df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
+
+from sklearn.preprocessing import StandardScaler
+
+features = ['sepal length','sepal width','petal length','petal width','target']
+
+# Separating out the features
+
+x = df.loc[:, features].values
+
+# Separating out the target
+
+y = df.loc[:, ['target']].values
+
+# Standardising the features
+
+x = StandardScaler().fit_transform(x)
+
