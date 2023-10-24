f81ea413315f4d06a303e6a707b922afc25d1f52
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2023-10-24 15:18:34 +0100

    Visualisation

--- PCA_example.py
+++ PCA_example.py
@@ -10,7 +10,7 @@ df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal 
 
 from sklearn.preprocessing import StandardScaler
 
-features = ['sepal length','sepal width','petal length','petal width','target']
+features = ['sepal length','sepal width','petal length','petal width']
 
 # Separating out the features
 
@@ -24,3 +24,33 @@ y = df.loc[:, ['target']].values
 
 x = StandardScaler().fit_transform(x)
 
+# PCA projection to 2D
+
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=3)
+
+principalComponents = pca.fit_transform(x)
+
+principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
+
+finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
+
+#Visualise 2D projection
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+
+targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
